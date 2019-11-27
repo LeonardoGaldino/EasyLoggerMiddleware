881e3e155a008e825ad7a79dd5d78141e1cedaab
Leonardo Galdino <lcgm@cin.ufpe.br>
2019-11-27 17:16:55 -0300

    Adds Ping to redis on InitLogger

--- internal/configuration/easylogger/configuration.go
+++ internal/configuration/easylogger/configuration.go
@@ -1,9 +1,16 @@
 package easylogger
 
+import "fmt"
+
 // Address is a struct containing a address information, e.g., host and port.
 type Address struct {
 	Host string
 	Port int
+}
+
+// FullAddress returns full address as string (host:port)
+func (addr *Address) FullAddress() string {
+	return fmt.Sprintf("%s:%d", addr.Host, addr.Port)
 }
 
 // Configuration is a struct containing addresses information
--- pkg/middleware/easylogger/core.go
+++ pkg/middleware/easylogger/core.go
@@ -1,8 +1,12 @@
 package easylogger
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/LeonardoGaldino/EasyLoggerMiddleware/internal/configuration"
 	"github.com/LeonardoGaldino/EasyLoggerMiddleware/internal/configuration/easylogger"
+	"github.com/gomodule/redigo/redis"
 )
 
 var (
@@ -12,11 +16,28 @@ var (
 )
 
 // InitLogger initializes logger with configuration file
-func InitLogger(loggerConfigsPath string) {
+func InitLogger(loggerConfigsPath string) error {
 	configsPath = loggerConfigsPath
 	configs := &easylogger.Configuration{}
-	configuration.LoadConfiguration(loggerConfigsPath, &configs)
+	err := configuration.LoadConfiguration(loggerConfigsPath, &configs)
+	if err != nil {
+		return err
+	}
+
 	redisService = configs.RedisService
 	namingService = configs.NamingService
 	isPackageSetup = true
+
+	conn, err := redis.Dial("tcp", redisService.FullAddress())
+	if err != nil {
+		return err
+	}
+
+	res, err := redis.DoWithTimeout(conn, time.Second*2, "PING", "Hello, Redis!")
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Redis server on, PING response: %+v\n", string(res.([]uint8)))
+	return nil
 }
