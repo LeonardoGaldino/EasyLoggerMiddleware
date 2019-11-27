b7af983cf841029fb4e536b16dd1a4541133b087
Leonardo Galdino <lcgm@cin.ufpe.br>
2019-11-27 17:24:01 -0300

    Makes namingService setup receive host and port as CLI argument

--- cmd/namingservice/namingservice.go
+++ cmd/namingservice/namingservice.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/LeonardoGaldino/EasyLoggerMiddleware/internal/configuration"
@@ -12,8 +13,8 @@ import (
 
 func main() {
 	numArgs := len(os.Args) - 1
-	if numArgs != 1 {
-		fmt.Printf("Wrong number of arguments. Expected 1 (config file path), got %d", numArgs)
+	if numArgs != 3 {
+		fmt.Printf("Wrong number of arguments. Expected 3 (ConfigFilePath, Host, Port), got %d", numArgs)
 		os.Exit(1)
 	}
 
@@ -23,11 +24,17 @@ func main() {
 		panic(err)
 	}
 
+	port64, err := strconv.ParseInt(os.Args[3], 10, 32)
+	if err != nil {
+		panic(err)
+	}
+	port := int(port64)
+
 	for _, config := range configs.Loggers {
 		fmt.Printf("%+v\n", config)
 	}
 
-	server := nsserver.InitNamingService("localhost", 8080, configs)
+	server := nsserver.InitNamingService(os.Args[2], port, configs)
 	server.Start(2)
 
 	time.Sleep(time.Hour)
